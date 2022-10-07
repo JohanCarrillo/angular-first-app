94cb2bdb9c47d76244be40b44dce8ad5cd5a545b
JohanCarrillo <johanime1907@gmail.com>
2022-10-07 12:11:38 -0500

    created first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MondongoComponent } from './mondongo/mondongo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MondongoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mondongo/mondongo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mondongo',
+  templateUrl: './mondongo.component.html',
+  styleUrls: ['./mondongo.component.scss']
+})
+export class MondongoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
